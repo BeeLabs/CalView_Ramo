0746dc67b6850a5487fa90255039c0155e5c9b68
Ahmed Abdulla <beelabs@gmail.com>
2021-12-11 12:26:25 +0300

    Deployed default example

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:calendar_view/calendar_view.dart';
 
+import 'model/event.dart';
+import 'pages/mobile/mobile_home_page.dart';
+import 'pages/web/web_home_page.dart';
+import 'widgets/responsive_widget.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,11 +14,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  CalendarControllerProvider(
-        controller: EventController(),
-        child: Scaffold(
-          body: WeekView(),
-        )// Your initialization for material app.
+    return CalendarControllerProvider<Event>(
+      controller: EventController<Event>(),
+      child: MaterialApp(
+        title: 'CalView-Ramo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        home: ResponsiveWidget(
+          mobileWidget: MobileHomePage(),
+          webWidget: WebHomePage(),
+        ),
+      ),
     );
   }
 }
